7cb23da05370289936ff2bd8f92db47758c1e84f
Gao Zhouhao <2996945797@qq.com>
2026-09-22 16:55:08 +0800

    Include verified archive and content IDs in video embeds

--- components/CompetitionVideo.tsx
+++ components/CompetitionVideo.tsx
@@ -1,9 +1,21 @@
-type Props = { bvid: string; title: string };
+// IDs verified against the public Bilibili video pages.
+// Supply the content ID as well as the BV ID for the mobile player.
+const videos = {
+  BV1hiQXBFEzB: { aid: "116274864592594", cid: "36898932460" },
+  BV1QXQQBbEMi: { aid: "116275065920784", cid: "36899196572" },
+} as const;
+
+type Props = { bvid: keyof typeof videos; title: string };
 
 export default function CompetitionVideo({ bvid, title }: Props) {
+  const { aid, cid } = videos[bvid];
+  const params = new URLSearchParams({
+    aid, bvid, cid, p: "1", page: "1",
+    autoplay: "0", poster: "1", danmaku: "0",
+  });
   return <div className="video-preview">
     <iframe
-      src={`https://player.bilibili.com/player.html?bvid=${bvid}&autoplay=0&poster=1&danmaku=0`}
+      src={`https://player.bilibili.com/player.html?${params}`}
       title={title}
       loading="eager"
       scrolling="no"
